9bc05e3eec649cd3d8e0f5d874cdd05082534129
LimeFallacie <e0014667@u.nus.edu>
2019-10-24 17:21:53 +0800

    boilerplate code

---
+++ frontend/src/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Card extends Component {
+    
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Card
---
+++ frontend/src/Error404.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Error404 extends Component {
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Error404
