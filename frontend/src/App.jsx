be66d06adaf1372560593c9efe75040aa27669fa
bokwoon <bokwoon.c@gmail.com>
2019-10-24 22:36:32 +0800

    add home styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,43 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Navbar from "./Navbar";
 import Home from "./Home";
 import Search from "./Search";
+import './App.css';
 
 export default function App() {
   return (
     <Router>
-      <Navbar />
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark d-flex justify-content-between">
+        {/* Left Align */}
+        <Link to="/" className="navbar-brand display-4">
+          React + API
+        </Link>
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item NAVBAR-ITEM">
+            <Link to="/" className="text-muted">Home</Link>
+          </li>
+          <li className="nav-item NAVBAR-ITEM">
+            <Link to="/search" className="text-muted">Search</Link>
+          </li>
+          <li className="nav-item"></li>
+        </ul>
+        {/* End Left Align */}
+
+        {/* Right Align */}
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item NAVBAR-ITEM">
+              <Link to="/" className="text-muted">Register</Link>
+            </li>
+            <li className="nav-item NAVBAR-ITEM">
+              <Link to="/" className="text-muted">Login</Link>
+            </li>
+            <li className="nav-item"></li>
+          </ul>
+        </div>
+        {/* End Right Align */}
+      </nav>
+
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/search" component={Search} />
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import './Home.css';
 
 export default function Home() {
-  return <div>This is Home</div>;
+  return (
+    <React.Fragment>
+      <div className="container-fluid d-flex align-items-center justify-content-center h-100">
+        <div className="row d-flex justify-content-center text-center">
+          <div className="col-md-5">
+            <h1 className="display-4 text-dark pt-5 mb-2 mt-5">Welcome to the React + API Challenge Landing Page</h1>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-center mt-4">
+        <button className="BTN btn-dark">REGISTER</button>
+        <button className="BTN btn-secondary">LOGIN</button>
+      </div>
+    </React.Fragment>
+  );
 }
