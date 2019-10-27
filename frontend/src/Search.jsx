63e8a0f24bc8ace7ec5a11da2ac12a1f096ccee6
LimeFallacie\Emil <e0014667@u.nus.edu>
2019-10-27 14:20:43 +0800

    search page stuff

--- frontend/src/Search.jsx
+++ frontend/src/Search.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Search() {
-    state = {
-        added:[],
-        searched:[]
-    }
-
-    textInput = () => {
-        fetch("https://api.github.com/search/users?q="+this.state.currentSearch+"&per_page=9")
+    
+    const [keyword, setKeyword] = useState("");
+    const [added, add] = useState([]);
+    const [found, searched] = useState([]);
+    
+    function searchText(keyword){
+        fetch("https://api.github.com/search/users?q="+keyword+"&per_page=9")
         .then(res=>res.json())
-        .then(data=>this.setState({userData:data.items}))
+        .then(data=>searched(data.items))
+        console.log(found)
     };
+    
 
-    render() {
-        return(
-            <div>
-                
-            </div>            
-        );
-    }
-}
-
-export default Search;
+    return(
+        <div>
+            <div className="container-fluid d-flex align-items-center justify-content-center h-100 mt-5 w-auto">
+                <input type="text" name='search' className='form-control' 
+                placeholder="Enter name" onChange={(e) => setKeyword(e.target.value)} 
+                style={{width : "200px"}}></input>
+                <button type="button" className="btn btn-secondary" onClick={ console.log(keyword), searchText(keyword) }>Go!</button> 
+            </div>
+            <div className="container-fluid d-flex align-items-center justify-content-center h-100">
+                <h4 className="my-5">No user stored in this profile.</h4>
+            </div>
+        </div>            
+    );
+    
+};
