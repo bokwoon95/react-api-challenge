c7073ad6801f2457427aa104081a4391bb88bf3c
LimeFallacie\Emil <e0014667@u.nus.edu>
2019-10-27 15:26:57 +0800

    Search bug in-progress

--- frontend/src/Search.jsx
+++ frontend/src/Search.jsx
@@ -8,29 +8,35 @@ export default function Search() {
     const [added, add] = useState([]);
     const [found, searched] = useState([]);
     
-    function searchText(word){
-        fetch("https://api.github.com/search/users?q="+word+"&per_page=9")
+    function searchText(keyword){
+        console.log(keyword)
+        fetch("https://api.github.com/search/users?q="+keyword+"&per_page=9")
         .then(res=>res.json())
-        .then(data=>searched(data.items))
+        .then(data=>console.log(data.items))
         console.log(found)
     };
     
     function renderCards(){
-        
+
     }
 
     return(
         <div>
             <div className="container-fluid d-flex align-items-center justify-content-center h-100 mt-5 w-auto">
-                <input type="text" name='search' className='form-control' 
-                placeholder="Enter name" onChange={(e) => setKeyword(e.target.value)} 
+                <input 
+                type="text"
+                className='form-control' 
+                placeholder="Enter name" 
+                onChange={(e) => setKeyword(e.target.value)} 
                 style={{width : "200px"}}></input>
-                <button type="button" className="btn btn-secondary" onClick={ console.log(keyword), searchText(keyword) }>Go!</button> 
+                <button 
+                type="button" 
+                className="btn btn-secondary" 
+                onClick={ searchText }>Go!</button> 
             </div>
             <div className="container-fluid d-flex align-items-center justify-content-center h-100">
                 <h4 className="my-5">No user stored in this profile.</h4>
             </div>
         </div>            
     );
-    
 };
